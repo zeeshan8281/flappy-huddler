79980ce9a14b294f1993ce5b944b31f3928d85de
Mohammad Zeeshan Jawed <zeeshan8281@gmail.com>
2025-06-03 12:50:59 +0530

    Update app title to Flappy Huddler

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Flappy Huddler',
+  description: 'Created with Flappy Huddler',
   generator: 'v0.dev',
 }
 
